3c19bab30d53dd98570f08dde04a5cb1ba42d16e
Logan Hennessy <loganjhennessy@gmail.com>
2017-10-11 11:40:14 -0700

    Changing the names to flaskbuzz.

--- app.py
+++ app.py
@@ -7,21 +7,34 @@ To run, execute the following
 
 '''
 
-from fizzbuzz_funcs import fizzbuzz_reg
+from flaskbuzz_funcs import flaskbuzz_reg
 from flask import Flask, request
 
 app = Flask(__name__) # instantiate the flask app
 
-@app.route('/fizzbuzz_basic') # define the endpoint
-def fizzbuzz_basic(): # does not need to match endpoint name, but makes it more readable if it does
-    fizzbuzz_list = fizzbuzz_reg()
-    return str(fizzbuzz_list) # flask requires str as output for endpoint methods
+@app.route('/flaskbuzz_basic') # define the endpoint
+def flaskbuzz_basic(): 
+    '''The method name does not need to match endpoint name, but makes
+    it more readable if it does.
 
-@app.route('/fizzbuzz_pro')
-def fizzbuzz_pro():
+    To access it, go to <ip>/flaskbuzz_basic
+
+    Where <ip> is the ip that your flask app is running on
+    '''
+    flaskbuzz_list = flaskbuzz_reg()
+    return str(flaskbuzz_list) # flask requires str as output for endpoint methods
+
+@app.route('/flaskbuzz_pro')
+def flaskbuzz_pro():
+    '''To access this, go to
+
+    <ip>/flaskbuzz_basic?start=<int>&end=<int>
+
+    Wehre <ipp> is the ip that your flask app is running on
+    '''
     start = request.args['start']
     end = request.args['end']
-    return str(fizzbuzz_reg(int(start),int(end)))
+    return str(flaskbuzz_reg(int(start),int(end)))
 
 if __name__ == '__main__':
     app.run()
--- flaskbuzz_funcs.py
+++ flaskbuzz_funcs.py
@@ -1,13 +1,13 @@
-def fizzbuzz_gen(start=1, end=100):
+def flaskbuzz_gen(start=1, end=100):
     pass
 
-def fizzbuzz_reg(start=1, end=100):
+def flaskbuzz_reg(start=1, end=100):
     output=[]
     for num in range(start, end+1):
         if num % 3 == 0 and num % 5 == 0:
-            output.append('fizzbuzz')
+            output.append('flaskbuzz')
         elif num % 3 == 0:
-            output.append('fizz')
+            output.append('flask')
         elif num % 5 == 0:
             output.append('buzz')
         else:
